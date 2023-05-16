3745243749d6db64f14c3de89421012dd917d622
izabelaapm <izabela.martins@germinare.org.br>
2023-05-16 16:38:29 -0300

    subiiindo

--- src/info.java
+++ src/info.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class info {
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class info {
+
+    private List<String> codigosDigitais = new ArrayList<>();
+
+    public void registrarAcesso(String codigoDigital) {
+        if (codigosDigitais.contains(codigoDigital)) {
+            System.out.println("Obrigado pela visita");
+        } else {
+            System.out.println("Bem-vindo, acesso liberado");
+            codigosDigitais.add(codigoDigital);
+        }
+    }
+
+    public void registrarSaida(String codigoDigital) {
+        if (codigosDigitais.contains(codigoDigital)) {
+            System.out.println("Saída registrada");
+            codigosDigitais.remove(codigoDigital);
+        } else {
+            System.out.println("Código digital não encontrado");
+        }
+    }
+
+    public String calcularTempoPermanencia(LocalDateTime dataEntrada, LocalDateTime dataSaida) {
+        long segundos = dataEntrada.until(dataSaida, java.time.temporal.ChronoUnit.SECONDS);
+        long minutos = segundos / 60;
+        segundos = segundos % 60;
+        return minutos + " minutos e " + segundos + " segundos";
+    }
+
 }
--- src/testeInfo.java
+++ src/testeInfo.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class testeInfo {
+import java.time.LocalDateTime;
+
+public class testeinfo {
+
+    public static void main(String[] args) {
+        info controle = new info();
+
+        // Teste 1 - Acesso liberado
+        System.out.println("Teste 1 - Acesso liberado");
+        String codigo1 = "123456";
+        LocalDateTime dataEntrada1 = LocalDateTime.now();
+        controle.registrarAcesso(codigo1);
+        LocalDateTime dataSaida1 = dataEntrada1.plusMinutes(5);
+        controle.registrarSaida(codigo1);
+        System.out.println("Tempo de permanência: " + controle.calcularTempoPermanencia(dataEntrada1, dataSaida1));
+
+        // Teste 2 - Acesso já registrado
+        System.out.println("Teste 2 - Acesso já registrado");
+        String codigo2 = "654321";
+        LocalDateTime dataEntrada2 = LocalDateTime.now();
+        controle.registrarAcesso(codigo2);
+        controle.registrarAcesso(codigo2);
+        LocalDateTime dataSaida2 = dataEntrada2.plusMinutes(10);
+        controle.registrarSaida(codigo2);
+        System.out.println("Tempo de permanência: " + controle.calcularTempoPermanencia(dataEntrada2, dataSaida2));
+
+        // Teste 3 - Código não encontrado
+        System.out.println("Teste 3 - Código não encontrado");
+        String codigo3 = "111111";
+        controle.registrarSaida(codigo3);
+    }
+
 }
